7fc9e3024b75ca7f5b2db7b7d430408a4e5cd17c
Restyled.io <commits@restyled.io>
2021-11-20 01:52:22 +0000

    Restyled by clang-format

--- codegen.js
+++ codegen.js
@@ -1,31 +1,32 @@
 module.exports = {
-    schema: [
-        {
-            'https://slate.hasura.app/v1/graphql': {
-                headers: {
-                    'x-hasura-admin-secret': "n4jRT12LJfD0ls3jS7G3i4qTdSMvDwt0fUsju5In8oEpCTAS0OJHpGibxHvOGNAV"
-                },
-            },
+  schema : [
+    {
+      'https://slate.hasura.app/v1/graphql' : {
+        headers : {
+          'x-hasura-admin-secret' :
+              "n4jRT12LJfD0ls3jS7G3i4qTdSMvDwt0fUsju5In8oEpCTAS0OJHpGibxHvOGNAV"
         },
-    ],
-    overwrite: true,
-    documents: ['./src/graphql/**/*.tsx', './src/graphql/**/*.ts'],
-    generates: {
-        './src/generated/graphql.tsx': {
-            plugins: [
-                'typescript',
-                'typescript-operations',
-                'typescript-react-apollo',
-            ],
-            config: {
-                skipTypename: false,
-                withHooks: false,
-                withHOC: false,
-                withComponent: false,
-            },
-        },
-        './graphql.schema.json': {
-            plugins: ['introspection'],
-        },
+      },
     },
+  ],
+  overwrite : true,
+  documents : [ './src/graphql/**/*.tsx', './src/graphql/**/*.ts' ],
+  generates : {
+    './src/generated/graphql.tsx' : {
+      plugins : [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      config : {
+        skipTypename : false,
+        withHooks : false,
+        withHOC : false,
+        withComponent : false,
+      },
+    },
+    './graphql.schema.json' : {
+      plugins : [ 'introspection' ],
+    },
+  },
 };
